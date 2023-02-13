f73b0c364ce88eefdfa0f86e4eb82cc9c8985e3d
Alek Luboński <aleklubonski@gmail.com>
2023-02-13 12:46:58 +0100

    Update in predict_iris.R names
    From not know to me reason the input object names should match the colum names from the model further investigation needed, but now the program works properly

--- predict_iris.R
+++ predict_iris.R
@@ -53,22 +53,22 @@ saveRDS(model, "iris_random_forest_model.rds")
       HTML("<h4>Select your iris parameter on sliders<h4>"),
       
       #Sepal length slider:
-      sliderInput("sepal_length", label = "Sepal length: ",
+      sliderInput("Sepal.Length", label = "Sepal length: ",
                   min = min(to_train$Sepal.Length),
                   max = max(to_train$Sepal.Length), value = 5.0),
       
       #Sepal width slider:
-      sliderInput("sepal_width", label = "Sepal width: ",
+      sliderInput("Sepal.Width", label = "Sepal width: ",
                   min = min(to_train$Sepal.Width),
                   max = max(to_train$Sepal.Width), value = 3.0),
       
       #Petal length slider:
-      sliderInput("petal_length", label = "Petal length: ",
+      sliderInput("Petal.Length", label = "Petal length: ",
                   min = min(to_train$Petal.Length),
                   max = max(to_train$Petal.Length), value = 5.0),
       
       #Petal width slider:
-      sliderInput("petal_width", label = "Petal width: ",
+      sliderInput("Petal.Width", label = "Petal width: ",
                   min = min(to_train$Petal.Width), 
                   max = max(to_train$Petal.Width), value = 0.3),
       
@@ -89,22 +89,22 @@ saveRDS(model, "iris_random_forest_model.rds")
 
 
   # Defining server function to run the app:
-  server <- function(input, output){
+  server <- function(input, output, session){
     
     # Inputting the data:
     data_set_input <- reactive({
       
       df <- data.frame(
         
-        Name = c("sepal_length",
-                 "sepal_width",
-                 "petal_length",
-                 "petal_width"),
+        Name = c("Sepal Length",
+                 "Sepal Width",
+                 "Petal Length",
+                 "Petal Width"),
         
-        Value = as.character(c(input$sepal_length,
-                               input$sepal_width,
-                               input$petal_length,
-                               input$petal_width)),
+        Value = as.character(c(input$Sepal.Length,
+                               input$Sepal.Width,
+                               input$Petal.Length,
+                               input$Petal.Width)),
         stringsAsFactors = FALSE
       )
       
@@ -117,7 +117,7 @@ saveRDS(model, "iris_random_forest_model.rds")
       
       test <- read.csv(paste("iris_predictor_input", ".csv", sep=''), header = TRUE)
       
-      # Making prediction about the species
+      # Making prediction about the species, name of the 
       Output <- data.frame(Prediction = predict(model, test),
                            round(predict(model, test, type = "prob"), 3))
       
